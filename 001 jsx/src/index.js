35cf6b0119ee4eaefb6f7e4a91a09e094b6d5483
Muthuvel <velmuthu37@gmail.com>
2022-06-29 12:40:08 +0530

    new project

--- 001 jsx/src/index.js
+++ 001 jsx/src/index.js
@@ -10,7 +10,11 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-  return <h1>{new Date().toLocaleTimeString()}</h1>;
+  const date = new Date();
+  const time = date.toLocaleString();
+
+  return <h1>{time}</h1>;
 }
+
 // 5) Show the component on the screen
 root.render(<App />);
