990e749d80224804d91ca982ba698edf2b630fc7
Muthuvel <velmuthu37@gmail.com>
2022-07-09 15:51:21 +0530

    <video>

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,14 @@
+import axios from axios
+
 const KEY = "AIzaSyDviWIF__QPNF7XxXumgviFBsNsG_G-b8Q";
+
+export default axios.create ({
+ baseURL: 'https://www.googleapis.com/youtube/v3',
+ params: {
+  part: 'snippet',
+  maxResults: 5,
+  type: 'video',
+  key: KEY,
+  q: 'surfboards'
+ }
+});
