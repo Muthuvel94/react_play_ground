d2bd76723ee1922148eaf50e1d8d43ad02595a56
Muthuvel <velmuthu37@gmail.com>
2022-07-13 13:49:11 +0530

    <widget>

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -18,7 +19,7 @@ const items = [
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
