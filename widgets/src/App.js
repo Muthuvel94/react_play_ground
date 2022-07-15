a0815bca3abba4d0314e1784d37d8f53d1e45ade
Muthuvel <velmuthu37@gmail.com>
2022-07-15 18:31:26 +0530

    <widget>

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/accordion";
 import Search from "./components/Search";
+import DropDown from "./components/DropDown";
 
 const items = [
   {
@@ -16,10 +17,26 @@ const items = [
     content: "you use react by creating components",
   },
 ];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Bluw",
+    value: "blue",
+  },
+];
+
 export default () => {
   return (
     <div>
-      <Search />
+      <DropDown options={options} />
     </div>
   );
 };
