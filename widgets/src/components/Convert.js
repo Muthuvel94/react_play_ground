cc16e325a662b15ba7bf9440b517681943a2741c
Muthuvel <velmuthu37@gmail.com>
2022-07-19 15:07:32 +0530

    <widget>

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -1,7 +1,42 @@
 import react, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
-  return <div />;
+  const [translated, setTranslated] = useState("");
+  const [deboundedText, setDeboundedText] = useState(text);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDeboundedText(text);
+    }, 500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: deboundedText,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
+  }, [language, deboundedText]);
+
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
