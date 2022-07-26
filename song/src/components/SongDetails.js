3457a15d2f7ba0c833011e4ae1273b5541604910
Muthuvel <velmuthu37@gmail.com>
2022-07-26 12:18:16 +0530

    <song_Redux>

--- song/src/components/SongDetails.js
+++ song/src/components/SongDetails.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { connect } from "react-redux/es/exports";
 
-const SongDetails = () => {
-  return <div>Song Details</div>;
+const SongDetails = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for : </h3>
+      <p>
+        {" "}
+        TItle: {song.title}
+        <br></br>
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
