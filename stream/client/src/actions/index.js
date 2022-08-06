5bcd0776fcc79dae733660b7c9892420f175e1e9
Muthuvel <velmuthu37@gmail.com>
2022-08-06 16:34:46 +0530

    <stream_Redux>

--- stream/client/src/actions/index.js
+++ stream/client/src/actions/index.js
@@ -1,4 +1,3 @@
-import { formValues } from "redux-form";
 import streams from "../apis/streams";
 import {
   SIGN_IN,
@@ -23,8 +22,9 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
--- stream/client/src/reducers/streamReducer.js
+++ stream/client/src/reducers/streamReducer.js
@@ -1,4 +1,4 @@
-import _, { omit } from "lodash";
+import _ from "lodash";
 import {
   CREATE_STREAM,
   FETCH_STREAM,
@@ -9,6 +9,8 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
