fc436d565ad974a5f1ee5c862141ab50a906261e
Muthuvel <velmuthu37@gmail.com>
2022-08-23 18:23:27 +0530

    <blog-post  old React-Redux>

--- Post Router/src/components/post_new.js
+++ Post Router/src/components/post_new.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Field, Form, reduxForm } from 'redux-form';
 
 class PostNew extends React.Component {
+  renderTitleField(field) {
+    return (
+      <div className='form-group'>
+        <label>Title</label>
+        <input className='form-control' type='text' {...field.input} />
+      </div>
+    );
+  }
   render() {
-    return <div>PostNew</div>;
+    return (
+      <Form>
+        <Field name='title' component={this.renderTitleField} />
+        <Field name='tags' component={this.renderTagsField} />
+      </Form>
+    );
   }
 }
-export default PostNew;
+export default reduxForm({ form: 'PostsNewForm' })(PostNew);
