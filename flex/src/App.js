35447f48d218d031d3aa23a3a921d729b6586778
Muthuvel <velmuthu37@gmail.com>
2022-11-25 13:10:53 +0530

    <css practice>

---
+++ flex/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../src/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className='container'>
+          <div className='block'>Div 1</div>
+          <div className='block'>Div 2</div>
+          <div className='block'>Div 3</div>
+          <div className='block'>Div 4</div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
