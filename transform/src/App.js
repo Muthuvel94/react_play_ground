c1d72572827eff8fb02a82a37cc1d9449f96de7f
Muthuvel <velmuthu37@gmail.com>
2022-11-28 15:10:03 +0530

    <css practice>

---
+++ transform/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../src/App.css';
+
+class App extends React.Component {
+  render() {
+    return <div className='rotate'>App</div>;
+  }
+}
+
+export default App;
