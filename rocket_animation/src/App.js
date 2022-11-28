29f0e90301e508024e127db29839577cf96efb8e
Muthuvel <velmuthu37@gmail.com>
2022-11-28 16:41:16 +0530

    <css practice>

---
+++ rocket_animation/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../src/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <img
+        alt='Rocket Vector'
+        src='https://www.shutterstock.com/image-vector/cartoon-rocket-space-ship-take-260nw-540835846.jpg'
+      />
+    );
+  }
+}
+
+export default App;
